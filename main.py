6466d7a82d65a51dc725c2df74bffca0d0790491
xys20071111 <xys20071111@hotmail.com>
2020-11-04 22:35:36 +0800

    重写大部分代码

--- main.py
+++ main.py
@@ -1,62 +1,102 @@
-import sys,requests,json
-#cookie = sys.argv[1]
-#cookies={'sc1':cookie}
-print("Lejiqolexue tools,version 0.0.1")
-if len(sys.argv) < 2:
-    print('Please give me a login token as argv[1]')
-    exit()
-token = sys.argv[1]
-cookies={'sc1':token}
-print("Token in using:"+token)
-print('(1) Pull a blog.\n(2) Get blog list.\n(3) Publish a blog.')
-print('(4) Get user info.\n(5) Modify a blog.')
-print('Please enter a number,if you want to exit,enter ^C')
-choice = int(input())
-if choice == 1:
-    blogid = input('Please enter a blog id:')
-    res = requests.get('http://blog.app.ljlx.com/rest/blog/cont.ashx?talent_id='+blogid,cookies=cookies)
-    summary = requests.post('http://blog.app.ljlx.com/rest/blog/gettalent.ashx',cookies=cookies,data={'tag':'1','page_size':'1','page_index':blogid})
-    print('Blog info:\n' + summary.text)
-    print('Blog content:\n' + res.text)
-    choice = input('Would you like to save its content to a JSON file? [y/n]')
-    if choice == 'y':
-        filename = input('Filename(without ".json"):')
-        with open(filename+'.json','w+') as f:
-            f.write(res.text)
-elif choice == 2:
-    typeId = input('Type id:')
-    pageSize = input('Page size:')
-    res = requests.get('http://blog.app.ljlx.com/rest/blog/gettalent.ashx',cookies=cookies,data={'tag':typeId,'page_index':'1','page_size':pageSize})
-    choice = input('Save it into a JSON file? [y/n]')
-    if choice == 'y':
-        filename = input('Filename(without ".json"):')
-        with open(filename+'.json','w+') as f:
-            f.write(res.text)
-    else:
-            print(res.text)
-elif choice == 3:
-        uid = input('UID:')
-        title = input('Title:')
-        summary = input('Summary:')
-        text = input('Text:')
-        data = {'title':json.dumps({'title':title,'summary':summary}),'user_id':uid,'proxy_user_id':'0','content':'{"list":[{"type":10,"content":"<type=\"text\",size=6,fontsize=45.00,textcolor=\"33,33,33\">' + text + '"}]}','tag':'1','type':'0'}
-        print(requests.post('http://blog.app.ljlx.com/rest/blog/addtalent.ashx',cookies=cookies,data=data).text)
+import sys
+import requests
+import json
 
-elif choice == 4:
-    uid = input('UID:')
-    res = requests.get('https://api.ljlx.com/platform/userinfo/getuserextinfo?user_id='+uid,cookies=cookies)
-    choice = input('Save it to a JSON file? [y/n]')
-    if choice == 'y':
-        filename = input('Filename(without .json):')
-        with open(filename+'.json','w+') as f:
-            f.write(res.text)
-    else:
+
+def user(commandList):
+    if(commandList[0]) == 'get':
+        uid = commandList[1]
+        res = requests.get(
+            'https://api.ljlx.com/platform/userinfo/getuserextinfo?user_id='+uid, cookies=cookies)
         print(res.text)
-elif choice == 5:
-    uid = input('Author\'s uid:')
-    blogid = input('Blog id:')
-    title = input('Title:')
-    summary = input('Summary:')
-    text = input('Text:')
-    data = {'title':json.dumps({'title':title,'summary':summary}),'user_id':uid,'proxy_user_id':uid,'content':'{"list":[{"type":10,"content":"<type=\"text\",size=6,fontsize=45.00,textcolor=\"33,33,33\">' + text + '"}]}','tag':'1','type':'0','talent_id':blogid}
-    print(requests.post('http://blog.app.ljlx.com/rest/blog/modifyblog.ashx',cookies=cookies,data=data).text)
+
+
+def blog(commandList):
+    command = commandList[0]
+    if command == 'get':
+        blogid = commandList[1]
+        res = requests.get(
+            'http://blog.app.ljlx.com/rest/blog/cont.ashx?talent_id='+blogid, cookies=cookies)
+        print(res.text)
+    elif command == 'list':
+        typeId = commandList[1]
+        pageSize = commandList[2]
+        res = requests.get('http://blog.app.ljlx.com/rest/blog/gettalent.ashx',
+                           cookies=cookies, data={'tag': typeId, 'page_index': '1', 'page_size': pageSize})
+        print(res.text)
+    elif command == 'publish':
+        uid = commandList[1]
+        btype = commandList[2]
+        tag = commandList[3]
+        title = commandList[4]
+        summary = commandList[5]
+        text = commandList[6]
+        data = {
+            'title': json.dumps({'title': title, 'summary': summary}),
+            'user_id': uid,
+            'proxy_user_id': uid,
+            'content': '{"list":[{"type":10,"content":"<type=\"text\",size=6,fontsize=45.00,textcolor=\"33,33,33\">' + text + '"}]}', 'tag': tag, 'type': btype}
+        print(requests.post('http://blog.app.ljlx.com/rest/blog/addtalent.ashx',
+                            cookies=cookies, data=data).text)
+    elif command == 'modify':
+        uid = commandList[1]
+        blogid = commandList[2]
+        title = commandList[3]
+        summary = commandList[4]
+        text = commandList[5]
+        data = {'title': json.dumps({'title': title, 'summary': summary}), 'user_id': uid, 'proxy_user_id': uid,
+                'content': '{"list":[{"type":10,"content":"<type=\"text\",size=6,fontsize=45.00,textcolor=\"33,33,33\">' + text + '"}]}', 'tag': '1', 'type': '0', 'talent_id': blogid}
+    print(requests.post('http://blog.app.ljlx.com/rest/blog/modifyblog.ashx',
+                        cookies=cookies, data=data).text)
+
+
+def help(commandList):
+    if len(commandList) == 0:
+        print('help <command> [subcommand]')
+        return
+    if len(commandList) == 1:
+        command = commandList[0]
+        if command == 'blog':
+            print('subcommand: get list publish modify')
+            return
+        elif command == 'user':
+            print('subcommand: get')
+            return
+    command = commandList[0]
+    subcommand = commandList[1]
+    if command == 'blog':
+        if subcommand == 'get':
+            print('blog get <BlogId>')
+            return
+        elif subcommand == 'list':
+            print('blog list <TypeId> <PageSize>')
+            return
+        elif subcommand == 'publish':
+            print('blog publish <uid> <TypeId> <Tag> <Title> <Summary> <Text>')
+        elif subcommand == 'modify':
+            print('blog modify <uid> <BlogId> <Title> <summary> <Text>')
+    elif command == 'user':
+        if subcommand == 'get':
+            print('user get <uid>')
+
+def run():
+    command = input('>')
+    commandList = command.split(' ')
+    if commandList[0] == 'blog':
+        blog(commandList[1:])
+    elif commandList[0] == 'user':
+        user(commandList[1:])
+    elif commandList[0] == 'help':
+        help(commandList[1:])
+    elif commandList[0] == 'exit' or commandList[0] == 'quit':
+        sys.exit(0)
+
+if __name__ == '__main__':
+    print("Lejiqolexue tools,version 0.0.1")
+    token = '4CE2959613CAEE0AE15240B5979612FED5656A11aU1%2fMgHtBdWR2DBcaQw%2bsRV7NBWwYAj%2ft1mUi4aPu0Y0QRXPnGlmNjO%2b8Q6QrRvxGOjimokBxGbI0CZfr3tUYbV3zL24juyFpgVWf1MMn2CBIqgG6iRLEGukmTm%2fTGLPU5scgYjg4Qrkn8sZL1vjn9LV63flzsj%2byR2tx6Ql'
+    if len(sys.argv) > 2:
+        token = sys.argv[1]
+    cookies = {'sc1': token}
+    print("Token in using:"+token)
+    while True:
+        run()
